a0bda24f7261089f4c2237957660897cc982a99b
Nubovik <50026919+Nubovik01@users.noreply.github.com>
2022-04-25 19:50:25 +0300

    Update anti-auth.user.js

--- extension/anti-auth.user.js
+++ extension/anti-auth.user.js
@@ -2,17 +2,11 @@
 // @name         Анти-авторизация на PornHub
 // @namespace    https://github.com/Nubovik01/anti-auth-pornhub/
 // @homepage     https://github.com/Nubovik01/anti-auth-pornhub/
-// @version      1.0.2
-// @encoding     utf-8
+// @version      1.0.3
 // @description  Этот скрипт убирает штучку "Авторизируйтесь через VK для подтверждения возраста"
 // @icon         https://di.phncdn.com/www-static/favicon.ico
 // @author       Nubovik
 // @match        *://*.pornhub.com/*
-// @compatible   chrome
-// @compatible   firefox
-// @compatible   opera
-// @compatible   safari
-// @compatible   edge
 // @downloadURL  https://github.com/Nubovik01/anti-auth-pornhub/raw/main/extension/anti-auth.user.js
 // @updateURL    https://github.com/Nubovik01/anti-auth-pornhub/raw/main/extension/anti-auth.user.js
 // @grant        GM_getResourceText
